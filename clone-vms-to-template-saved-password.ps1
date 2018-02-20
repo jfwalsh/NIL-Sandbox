06cf83109b9bbc86414d46bafe86a42ad6a5cf2e
jfwalsh <jfwalsh@users.noreply.github.com>
2018-02-20 10:31:17 +0000

    Add MGMT network to list of selectable traget networks
    Increase WG network range to WG-25. 
    Re-order code so all grlobal settings are at the top mof the file.

--- clone-vms-to-template-saved-password.ps1
+++ clone-vms-to-template-saved-password.ps1
@@ -4,10 +4,62 @@
 
 #### Method: clone VM to template folder, then instead of making a snapshot, use Set-VM to make it a template
 
-$VersionText = "Version 0.1 (2017-05-25)"
+$VersionText = "Version 0.5 (2018-02-20)"
 $Author      = "John Walsh | jwalsh@alienvault.com"
+$DataCenterName = "AV"  			# Name of Datacenter in vCenter
+$myServerName = "awc.nil.com"  			# Put vCenter FQDN or IP here
+$numberOfWorkGroups = 25 			# How many workgroups? WG-01 to WG-nn
+$additionalWorkGroups = @("MGMT")   		# Add any additional networks that might be used here 
 
+## WARNING - putting user credentials in this file is a security hazard. Ensure nobody else can read this file
+# vCenter credentials
+$myUsername = "PUT YOUR VCENTER USERNAME HERE"
+$myPassword = "PUT YOUR VCENTER PASSWORD HERE" 
 
+#### Make sure to stop script if any errors occur - the default is to continue.
+$ErrorActionPreference = "Stop"
+
+## Function definitions
+
+## Define a function to ask user to select a VM folder - enhanced to show full path
+## Requires a connection to vCenter
+function Select-VmFolder ([String] $Title){
+    # Get datacenter object, hard coded for DC "AV" in AWC
+	$dataCenter = Get-DataCenter -Name $DataCenterName 
+	# Get top level hidden vm folder called "vm" in selected DC
+	$vmFolder = Get-Folder -Type VM -Location $dataCenter -Name "vm" -NoRecursion
+
+	# Get all user-visible VM folders, this is recursive by default
+	$allVMFolders = Get-Folder -Location $vmFolder | sort
+
+	# Run through all folders, and add new member to each folder object called FullPath which
+	# is a string with the path to the folder, built from folder names and '|' separators.
+	$allVMFolders | % {
+		$folder = $_
+		
+		$f = $folder		# $f starts at current folder and then loops through parent folders
+		$path = $f.Name		# $path is the string showing the full path including any parent folder names
+
+		# While the parent folder is not the root "vm" folder work up and build path string
+		while ($f.Parent -ne $vmFolder) {
+			# Add parent name to front of $path
+			$path = $f.Parent.Name + " | " + $path
+			$f = $f.Parent
+		}
+		# Once path has been generated, add it to the $folder object as a NoteProperty
+		Add-Member -inputObject $folder -NotePropertyName "FullPath" -NotePropertyValue $path 
+	}
+
+	# Extract folder name, path and Id and feed to ogv.  We need the Id to retrieve the folder again
+	# as the folder name is ambiguous.
+	$selected = $allVMFolders | Select Name,FullPath,Id | ogv -OutputMode Single -Title $Title
+
+	$selectedFolder = Get-Folder -Id $selected.Id
+
+	$selectedFolder  # return this
+}
+
+#  If you get warnings about deprecated calls, try the following in your environment ...
 #### http://www.pragmaticio.com/2015/01/vmware-powercli-suppress-vcenter-certificate-warnings/
 # PowerCLI> Set-PowerCLIConfiguration -InvalidCertificateAction ignore -DisplayDeprecationWarnings:$false -confirm:$false
 #
@@ -19,21 +71,12 @@ $Author      = "John Walsh | jwalsh@alienvault.com"
 # AllUsers                                     Ignore                    False
 #
 
-#### GLOBALS ####
 
-$myServerName = "awc"  	# Put vCenter IP or hostname here 
-						# awc defined in local hosts file to be 192.168.252.141
-
-# Array of valid portgroups (networks) that can be selected for new VMs - change this as needed
-$numberOfWorkGroups = 20
 #### Build array of valid workgroup names, "WG01", WG02", etc.
 # https://social.technet.microsoft.com/wiki/contents/articles/7855.powershell-using-the-f-format-operator.aspx
-$WGNames = @()	# initialise to an empty array
-for ( $i=1 ;  $i -le $numberOfWorkGroups ; $i++ ) { $WGNames += ("WG-{0,2:d2}" -f $i) }
-
-
-#### Make sure to stop script if any errors occur - the default is to continue.
-$ErrorActionPreference = "Stop"
+$WGNames = @()								# initialise to an empty array
+$WGNames += $additionalWorkGroups  			# Prefix list of WG-nn networks with MGMT, etc.
+for ( $i=1 ;  $i -le $numberOfWorkGroups ; $i++ ) { $WGNames += ("WG{0,2:d2}" -f $i) }
 
 #### Connect to vCenter
 
@@ -45,14 +88,8 @@ Try {
 	Write-Host "Looks like there was no existing connection - continuing."
 }
 
-## WARNING - putting user credentials in this file is a security hazard. Ensure nobody else can read this file
-
-# vCenter credentials
-$myUsername = "put your username here"
-$myPassword = "put your password here"  
-
+# Create VC credential object with username and password
 $password = ConvertTo-SecureString $myPassword -AsPlainText -Force   # cannot use password directly - convert to secure string first
-
 $myCred =  New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $myUsername, $password
 
 # Connect to vCenter 
@@ -60,12 +97,12 @@ Write-Host "Connecting to vCenter Server $myServerName"
 $mySession = Connect-VIServer -Server $myServerName -Credential $myCred
 
 # Ask user to select a single folder where templates are located
-# The "-Type VM" is correct - this is to filter out ESX, datastore, network etc. folders. 
-$mySourceVMFolder = Get-Folder -Type VM | Sort-Object | ogv -OutputMode Single -Title "Select Source VM Folder"
+# 
+$mySourceVMFolder = Select-VmFolder -Title "Select Source VM Folder"
 
 
 # Ask user to select a target VM folder - this must already exist
-$myVmFolder = Get-Folder -Type VM | Sort-Object | ogv -OutputMode Single -Title "Select Target Folder for deployed VMs"
+$myVmFolder = Select-VmFolder -Title "Select Target Folder for deployed VMs"
 $myVmFolderName = $myVmFolder.Name
 $myVmFolderMoRef = ($myVmFolder | Get-View).MoRef 
 
@@ -129,7 +166,12 @@ $mySourceVMs | % {				# For each VM
 			# Check if current name matches WGnn-nn format
 			# If so, replace WGnn with target workgroup, leaving -nn part the same.
 			# Example: WG01-03 could become WG12-03.
-			if ($myOldNetworkName -match 'WG\d\d-\d\d') {
+			if ($myWorkGroup -eq "MGMT") {  # change network to MGMT regardless of current value
+				$myNewNetworkName = $myWorkGroup
+				Write-Host "Changing  $myAdapterName on $myVMName from $myOldNetworkName to $myNewNetworkName"
+				$discard = $myNetworkAdapter | Set-NetworkAdapter -NetworkName $myNewNetworkName -Confirm:$false
+			}
+			elseif ($myOldNetworkName -match 'WG\d\d-\d\d') { # map WGnn-xx networks
 				$myNewNetworkName = $myOldNetworkName -replace 'WG\d\d',$myWorkGroup
 				Write-Host "Changing  $myAdapterName on $myVMName from $myOldNetworkName to $myNewNetworkName"
 				$discard = $myNetworkAdapter | Set-NetworkAdapter -NetworkName $myNewNetworkName -Confirm:$false
@@ -138,7 +180,6 @@ $mySourceVMs | % {				# For each VM
 	}
 	
 	#### Now convert VM to a template
-	Write-Host "Converting $myVMName to a template ..."
 	$discard = Set-VM -VM $newVM -ToTemplate -Confirm:$false
 }
 
